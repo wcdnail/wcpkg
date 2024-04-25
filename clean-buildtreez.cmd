cd9a6b400b7275abc55d1849b6f7454760828ccf
Mike Nitro <wcdnail@gmail.com>
2024-04-25 21:44:51 +0300

    assist batch fix

--- clean-buildtreez.cmd
+++ clean-buildtreez.cmd
@@ -2,12 +2,12 @@
 
 echo Clean VCPKG buildtrees directory: %VCPKG_ROOT%
 
-rem FORFILES /P "%VCPKG_ROOT%\buildtrees" /S /M x64-windows* /C "cmd /c echo @path"
+rem FORFILES /P "%VCPKG_ROOT%\buildtrees" /S /M x64-* /C "cmd /c echo @path"
 
 setlocal
 setlocal enabledelayedexpansion
 @echo off
-for /d /r "%VCPKG_ROOT%\buildtrees" %%i in (x64-windows*) do (
+for /d /r "%VCPKG_ROOT%\buildtrees" %%i in (x64-*) do (
   set _dir=%%i
   echo Removing !_dir! ...
   @echo on
