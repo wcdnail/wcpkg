2de4201f76024e978ad09fbcbf03cf0387d53386
Mike Nitro <wcdnail@gmail.com>
2024-05-27 20:11:25 +0300

    clipp: switch to wcdnail fork

--- ports/clipp/portfile.cmake
+++ ports/clipp/portfile.cmake
@@ -1,11 +1,11 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO "muellan/clipp"
-    REF 2c32b2f1f7cc530b1ec1f62c92f698643bb368db
-    SHA512 4645fafe85a8099ea97b85e939747a12e9b3b92213b5b8207a9c277537377b77b5daebd88a4c090ea89cfff2937a9fc155da6e8b5558574d7129227c28826e1c
+    REPO "wcdnail/clipp"
+    REF master
+    SHA512 a9d0269aa2127843f1b85984aeacb331a01c358c5ad11bc985122c1f7ff5ff214b9fa2d75136589113692d47c7fad00de7214030356587bfd6ab95caa7c68f48
     HEAD_REF master
     PATCHES
-        Fix-result_of-deprecated-in-C++20.patch # https://github.com/muellan/clipp/pull/54
+        #Fix-result_of-deprecated-in-C++20.patch # https://github.com/muellan/clipp/pull/54
 )
 
 vcpkg_cmake_configure(
