4b523a32f939e062e3a8c3677c3cc630135f4a14
Mike Nitro <wcdnail@gmail.com>
2024-05-30 11:49:09 +0300

    clipp: update to 1.2.4 tag

--- ports/clipp/portfile.cmake
+++ ports/clipp/portfile.cmake
@@ -1,11 +1,9 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO "wcdnail/clipp"
-    REF master
-    SHA512 8b8cbf7af3ff8363e9bce462927f736d343cc2f712130db1ad4aa509f7c20243a8b63aa59e0f9c0c7dda936c20d45df32aa7cb9a6508fc36810389fc6c8f68c0
+    REF "${VERSION}"
+    SHA512 7033666ca8548d85bb924e6ca9099c4cff36274d50048af8fe6be31e120939cbfd54baea0ed0dc8e8915bac8c68aae824454cb41a931b2202e8e914c235a6a26
     HEAD_REF master
-    PATCHES
-        #Fix-result_of-deprecated-in-C++20.patch # https://github.com/muellan/clipp/pull/54
 )
 
 vcpkg_cmake_configure(
